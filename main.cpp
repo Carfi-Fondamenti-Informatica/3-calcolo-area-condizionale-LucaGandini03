6953057b57404d0fe6cba422df3e1b3819d7e94a
LucaGandini03 <116078651+LucaGandini03@users.noreply.github.com>
2022-11-20 20:41:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@ int main() {
    cin>>a>>b>>opzione;
    switch(opzione){
       case 0:
-          cout<<(a*b)/2;
+          cout<<(a*b)/2<<endl;
           break;
       case 1:
-         cout<<(a*a);
+         cout<<(a*a)<<endl;
          break;
       case 2:
-         cout<<(a*b);
+         cout<<(a*b)<<endl;
          break;
       default:
-         cout<<"opzione non valida";
+         cout<<"opzione non valida"<<endl;
    }
    return 0;
 }
